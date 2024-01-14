b68a67c228ae5d36dd000ced0853cbd7ecdbeb3c
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-01-14 12:54:40 -0700

    WEBGL: Extract constants to a separate file.

---
+++ webgl/src/MathUtils/Constants.ts
@@ -0,0 +1,10 @@
+/** Adaptive rendering constant. */
+export const pixelResolution = 0.75;
+/** Zoom factor before remeshing */
+export const zoomRemeshFactor = 1.5;
+export const FillFactor = 0.1;
+export const windowTrim = 10;
+export const third = 1 / 3;
+export const pi = Math.acos(-1.0);
+export const radians = pi / 180.0;
+export const maxDepth = Math.ceil(1 - Math.log2(Number.EPSILON));
