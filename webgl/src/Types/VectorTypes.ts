eb2e154b89a000825fb364548096d59c6781c5c5
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-01-14 12:36:23 -0700

    WEBGL: Add helper function for vec3 and vec2.

--- webgl/src/Types/VectorTypes.ts
+++ webgl/src/Types/VectorTypes.ts
@@ -4,6 +4,10 @@ export type Vector3 = {
   z: number;
 };
 
+export function vec3(x: number, y: number, z: number): Vector3 {
+  return { x, y, z };
+}
+
 export function v3ToArray(color: Vector3): Float32Array {
   return new Float32Array([color.x, color.y, color.z]);
 }
@@ -12,3 +16,7 @@ export type Vector2 = {
   x: number;
   y: number;
 };
+
+export function vec2(x: number, y: number): Vector2 {
+  return { x, y };
+}
